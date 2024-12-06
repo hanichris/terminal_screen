4c635b5297cfe6c6ca2b90a3febbac3eeabe02bd
hanichris <hanichris71@gmail.com>
2024-12-06 19:53:42 +0300

    define the function api for disassembly

--- src/include/debug.h
+++ src/include/debug.h
@@ -3,7 +3,21 @@
 
 #include "chunk.h"
 
+/**
+ * disassembleChunk - Disassembles the stream of bytes stored
+ * internally in a chunk.
+ * @chunk: a pointer to a 'Chunk' type.
+ * @name: header for the disassembly operation.
+ * @Returns: void.
+ */
 void disassembleChunk(Chunk* chunk, char const* name);
+/**
+ * disassembleInstruction - Disassembles each instruction in the
+ * stream of bytes.
+ * @chunk: a pointer to a 'Chunk' type.
+ * @offset: iterator.
+ * Returns: the 'offset' of the next instruction.
+ */
 unsigned disassembleInstruction(Chunk* chunk, unsigned offset);
 
 #endif
