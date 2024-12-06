1809c6078929e9648f5ba38362722ff18ffc38f5
hanichris <hanichris71@gmail.com>
2024-12-06 19:57:40 +0300

    add commentary about the header file

--- src/include/chunk.h
+++ src/include/chunk.h
@@ -25,8 +25,29 @@ typedef struct chunk_t {
 	uint8_t* code;
 } Chunk;
 
+/**
+ * initChunk - Initialises a chunk type to ensure it is in a valid
+ * state.
+ * @chunk: Pointer to a 'Chunk' type.
+ * @Returns: void.
+ **/
 void initChunk(Chunk* chunk);
+/**
+ * freeChunk - frees up the memory used to allocate the array to hold
+ * the stream of bytes.
+ * @chunk: pointer to a 'Chunk' type.
+ * @Returns: void.
+ */
 void freeChunk(Chunk* chunk);
+/**
+ * writeChunk - Appends a byte to the end of a chunk. If the array to
+ * store the bytes has capacity for a new byte, the byte is appended
+ * to the end of the array. If not, the internal array's capacity is increased
+ * before a new byte can be appended to it.
+ * @chunk: pointer to a 'Chunk' type.
+ * @byte: the byte that is to be appended to the array.
+ * @Returns: void.
+ */
 void writeChunk(Chunk* chunk, uint8_t byte);
 
 
