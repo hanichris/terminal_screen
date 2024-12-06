b164aa8d5f4360238eb3c3e73d703d4901f536d7
hanichris <hanichris71@gmail.com>
2024-12-06 19:58:11 +0300

    test vm's operation.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include "include/chunk.h"
 #include "include/debug.h"
+#include "include/vm.h"
 
 int main(void) {
+	VM vm = { };
 	Chunk chunk = { };
 	writeChunk(&chunk, EOS);
 	disassembleChunk(&chunk, "test chunk");
+	interpret(&vm, &chunk);
+	freeVM(&vm);
 	freeChunk(&chunk);
 	return 0;
 }
