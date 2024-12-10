dcbed0baff2e8c4ebbf19fea2799e232bcbf54f5
hanichris <hanichris71@gmail.com>
2024-12-10 18:55:17 +0300

    turn off debug trace and include the stdlib header file.

--- src/include/common.h
+++ src/include/common.h
@@ -1,10 +1,11 @@
 #if !defined(common_h)
 #define common_h
 
-#define DEBUG_TRACE_EXECUTION
+//#define DEBUG_TRACE_EXECUTION
 
 #include <err.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <sysexits.h>
 
