1e7a452d3d935eb62ec1784c6cc1283ce4253b2d
hanichris <hanichris71@gmail.com>
2024-12-11 01:53:35 +0300

    Implement a repl to read in user input.

--- test/main.c
+++ test/main.c
@@ -5,12 +5,33 @@
 #include <sys/types.h>
 #include <sysexits.h>
 
+#include "compiler.h"
+
+
 static void repl() {
-	printf("RUNNING A REPL\n");
+	char* line = nullptr;
+	size_t linecap = 0;
+
+	for (;;) {
+		printf("> ");
+		if (getline(&line, &linecap, stdin) == -1) {
+			if (feof(stdin)) {
+				break;
+			} else {
+				perror("readline");
+				exit(EXIT_FAILURE);
+			}
+		}
+		compile(line);
+		free(line);
+		line = nullptr;
+		linecap = 0;
+	}
+	printf("\n");
 }
 
 
-int main(int argc, [[maybe_unused]]char* argv[argc + 1]) {
+int main(int argc, [[maybe_unused]]char* argv[argc]) {
 	switch (argc) {
 		case 1:
 			repl();
