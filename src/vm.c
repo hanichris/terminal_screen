1e2afb88cf9e1add2b4aa4640ae6a736a69221a0
hanichris <hanichris71@gmail.com>
2024-12-10 18:58:16 +0300

    initialise a screen object. Process the 'SS', 'DAC', 'DC', and 'CM' instructions.

--- src/vm.c
+++ src/vm.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
+
 #include "include/debug.h"
+#include "include/memory.h"
+#include "include/screen.h"
 #include "include/vm.h"
 
 void initVM(VM* vm) {
-	vm->ip = nullptr;
-	initChunk(vm->chunk);
-	initScreen(vm->screen);
+
 }
 
 void freeVM(VM* vm) {
-
+	freeScreen(vm->screen);
 }
 
 static InterpretResult run(VM* vm) {
@@ -22,6 +24,48 @@ static InterpretResult run(VM* vm) {
 		switch (instruction) {
 			case EOS:
 				return INTERPRET_OK;
+			case SS: {
+					 [[maybe_unused]]uint8_t length = READ_BYTE();
+					 uint8_t screenWidth = READ_BYTE();
+					 uint8_t screenHeight = READ_BYTE();
+					 ColourMode colourMode = READ_BYTE();
+					 screenSetup(
+							 vm->screen, 
+							 screenWidth, 
+							 screenHeight, 
+							 colourMode
+					);
+					 break;
+				}
+			case DC: {
+					 if (vm->screen) {
+						 [[maybe_unused]]uint8_t length = READ_BYTE();
+						 uint8_t xCoord = READ_BYTE();
+						 uint8_t yCoord = READ_BYTE();
+						 uint8_t colourIndex = READ_BYTE();
+						 uint8_t character = READ_BYTE();
+						 drawCharacter(vm->screen, xCoord, yCoord, colourIndex, character);
+					 }
+					 break;
+				 }
+			case CM: {
+					 if (vm->screen) {
+						 [[maybe_unused]]uint8_t length = READ_BYTE();
+						 uint8_t xCoord = READ_BYTE();
+						 uint8_t yCoord = READ_BYTE();
+						 setCursor(vm->screen, xCoord, yCoord);
+					 }
+					 break;
+				 }
+			case DAC: {
+					  if (vm->screen) {
+						  [[maybe_unused]]uint8_t length = READ_BYTE();
+						  uint8_t character = READ_BYTE();
+						  uint8_t colourIndex = READ_BYTE();
+						  drawAtCursor(vm->screen, character, colourIndex);
+					  }
+					  break;
+				  }
 		}
 	}
 #undef READ_BYTE
@@ -30,5 +74,8 @@ static InterpretResult run(VM* vm) {
 InterpretResult interpret(VM *vm, Chunk *chunk) {
 	vm->chunk = chunk;
 	vm->ip = chunk->code;
+	vm->screen = GROW_ARRAY(Screen, vm->screen, 0, 1);
+	initScreen(vm->screen);
+	printf("\x1b[1J\x1b[H");
 	return run(vm);
 }
